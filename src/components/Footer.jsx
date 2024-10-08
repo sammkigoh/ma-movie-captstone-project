207a5fe415fd62ed26af5f24e962387a60601509
Samm <sammkigoh@gmail.com>
2024-10-08 13:53:24 +0300

    UI improvements on the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="bg-transparent text-white p-4 text-center">
+		<footer className="bg-transparent text-current p-4 text-center">
 			<p className="text-sm">
 				&copy; {new Date().getFullYear()} Ma-Movie. All Rights Reserved.{" "}
 			</p>
 			<div className="mt-2">
-				<a href="/about" className="text-white hover:underline mx-2">
+				<a href="/about" className="text-current hover:underline mx-2">
 					About Me
 				</a>
 				<a
 					href="https://policies.google.com/privacy?hl=en-US"
-					className="text-white hover:underline mx-2"
+					className="text-current hover:underline mx-2"
 				>
 					Privacy Policy
 				</a>
 				<a
 					href="https://policies.google.com/terms?hl=en-US"
-					className="text-white hover:underline mx-2"
+					className="text-current hover:underline mx-2"
 				>
 					Terms of Service
 				</a>
